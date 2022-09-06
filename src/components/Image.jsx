033a9925f7f58c45d3eee9d680ec5ad5dc739f99
ol3gatron <olegatron@gmail.com>
2022-09-06 19:02:55 +0300

    added heart and cart icon

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+
 function Image({className, image}) {
-    return (
-        <div className={`${className} image-container`}>
-            <img src={image.url} className="image-grid"/>
-        </div>
-    )
+  const [isHovered, setIsHovered] = useState(false)
+
+  const heartIcon = isHovered && <i className="ri-heart-line favorite"></i>
+  const cartIcon = isHovered && <i className="ri-add-circle-line cart"></i>
+
+  return (
+      <div
+        className={`${className} image-container`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
+
+      {heartIcon}
+      {cartIcon}
+
+      <img src={image.url} className="image-grid"/>
+      </div>
+  )
 }
 
 export default Image
--- src/pages/Photos.jsx
+++ src/pages/Photos.jsx
@@ -3,11 +3,6 @@ import Image from "../components/Image"
 import { PicsomeContext } from "../PicsomeContext"
 import { useContext } from "react"
 
-// Get the allPhotos array from context
-    // map over it, creating <Image /> elements of the component we just made
-    // <Image key={???} img={<full image object here>} className={getClass(<index of image>)} />
-
-
 function Photos() {
   const {images} = useContext(PicsomeContext)
   const imagesElements = images.map((image, index) => {

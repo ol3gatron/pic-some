f5891302c8b13aa13ead0b9a434cd30a60ea8bd0
ol3gatron <olegatron@gmail.com>
2022-09-09 12:49:27 +0300

    created cart item component

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,23 @@
-import React from "react"
+import { useContext, useState } from "react"
+import { PicsomeContext } from "../PicsomeContext"
+
+import CartItem from "../components/CartItem"
+
 
 function Cart() {
+  const {cartItems} = useContext(PicsomeContext)
+  const cartItemElements = cartItems.map(item => (
+    <CartItem key={item.id} item={item} />
+  ))
+
   return (
     <main className="cart-page">
       <h1>Check out</h1>
+      {cartItemElements}
+      <p className="total-cost">Total: </p>
+      <div className="order-button">
+        <button>Place Order</button>
+      </div>
     </main>
   )
 }
